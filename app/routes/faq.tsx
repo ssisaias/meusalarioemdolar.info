212d65bb50bd1e5c9ce95bd03c117d046f5b88de
ssisaias <ssisaias@users.noreply.github.com>
2024-10-10 21:55:28 -0300

    faq

--- app/routes/faq.tsx
+++ app/routes/faq.tsx
@@ -11,15 +11,38 @@ export default function Index() {
   return (
     <div className="min-h-screen bg-white p-8">
       <div className="max-w-4xl mx-auto space-y-4">
-
         <section className="space-y-4">
           <h1 className="text-3xl font-bold">FAQ (PT-Br)</h1>
           <Accordion type="single" collapsible>
             <AccordionItem value="item-1">
               <AccordionTrigger>Qual a motivação?</AccordionTrigger>
               <AccordionContent>
-                <p>1. Ter um local direto para me lembrar sempre a merda que é ganhar em real.</p>
+                <p>
+                  1. Ter um local direto para lembrar-me sempre a merda que é
+                  ganhar em real.
+                </p>
                 <p>2. Experimentar o framework Remix e utilizar em produção</p>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+              <AccordionTrigger>Stack utilizada:</AccordionTrigger>
+              <AccordionContent>
+                <ul className="list-disc pl-4">
+                  <li>Remix</li>
+                  <li>Vite</li>
+                  <li>zustand</li>
+                </ul>
+                <p className="mt-4">
+                  O código fonte está disponível para contribuição em{" "}
+                  <Link
+                    to={"https://github.com/ssisaias/meusalarioemdolar"}
+                    className="underline"
+                  >
+                    https://github.com/ssisaias/meusalarioemdolar
+                  </Link>
+                </p>
               </AccordionContent>
             </AccordionItem>
           </Accordion>
