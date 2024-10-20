2bbeac7285c7dfaf2aacf57bc549b3133a01382b
ssisaias <ssisaias@users.noreply.github.com>
2024-10-20 16:12:43 -0300

    removes unused script and database

--- scripts/database/setup.ts
+++
@@ -1,9 +0,0 @@
-import Database from 'better-sqlite3';
-
-const db = new Database('database.db');
-
-// Create a table if it doesn't exist
-db.exec(`CREATE TABLE IF NOT EXISTS scraped_data (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    content TEXT
-)`);
