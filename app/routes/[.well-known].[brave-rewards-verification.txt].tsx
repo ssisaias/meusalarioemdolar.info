0f242179a1b120623373c79960af16c0086a697b
ssisaias <ssisaias@users.noreply.github.com>
2024-12-04 20:05:38 -0300

    code fix

--- app/routes/[.well-known].[brave-rewards-verification.txt].tsx
+++ app/routes/[.well-known].[brave-rewards-verification.txt].tsx
@@ -3,7 +3,7 @@ export const loader = () => {
     const robotText = `This is a Brave Creators publisher verification file.
 
 Domain: meusalarioemdolar.info
-Token: 02cd5597f5c014d5ca4c298b08f62fb8e7946475fae287419b655cf75a98d48f
+Token: b620c613cc0c7f64fe4a82d0ed12caa1379129c981c7c65a55773153f5193900
 `;
     return new Response(robotText, {
       status: 200,
