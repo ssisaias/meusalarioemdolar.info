098c38fd8cb8d878837e0b98a703f2eab14b394f
ssisaias <ssisaias@users.noreply.github.com>
2025-12-11 16:39:50 -0300

    database interface improvements

--- app/db/interface.ts
+++ app/db/interface.ts
@@ -1,83 +1,89 @@
-import Database from "better-sqlite3";
-import * as cheerio from "cheerio";
+import Database from 'better-sqlite3'
+import * as cheerio from 'cheerio'
 
-const dbEnv = process.env.DATABASE || ":memory:";
+const dbEnv = process.env.DATABASE || ':memory:'
 
-const db = new Database(dbEnv);
+const db = new Database(dbEnv)
 
-db.pragma("journal_mode = WAL");
+db.pragma('journal_mode = WAL')
 
 db.exec(`CREATE TABLE IF NOT EXISTS scraped_data (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     content TEXT,
     creationDate TEXT
-)`);
+)`)
 
 interface ScrapedData {
-  id: number;
-  content: string;
-  creationDate: string;
+  id: number
+  content: string
+  creationDate: string
 }
 
-interface ConversionRate {
-  source: string;
-  rate: number;
-  lastUpdateStr: string;
+interface UsdToBrlRate {
+  rate: number
+  lastUpdateStr: string
 }
 
 export function insertScrapedData(content: string): void {
   const stmt = db.prepare(
-    "INSERT INTO scraped_data (content, creationDate) VALUES (?, ?)"
-  );
-  stmt.run(content, new Date().toISOString());
-  console.log("Scraped data saved...");
+    'INSERT INTO scraped_data (content, creationDate) VALUES (?, ?)',
+  )
+  stmt.run(content, new Date().toISOString())
+  console.log('Scraped data saved...')
 }
 
-export async function getScrapedData(): Promise<ConversionRate | null> {
+export async function getScrapedData(): Promise<UsdToBrlRate | null> {
   const stmt = db.prepare(
-    "SELECT * FROM scraped_data ORDER BY creationDate DESC"
-  );
-  const allResult = stmt.all() as ScrapedData[];
+    'SELECT * FROM scraped_data ORDER BY creationDate DESC LIMIT 2',
+  )
+  const queryResult = stmt.all() as ScrapedData[]
 
-  let latest: ScrapedData;
-  if (!allResult.length) {
+  let latest: ScrapedData
+  if (!queryResult.length) {
     latest = {
       content: await fetchLatest(),
       creationDate: new Date().toISOString(),
       id: 0,
-    };
+    }
   } else {
-    latest = allResult[0];
+    latest = queryResult[0]
   }
 
-  // if its been more than 6 hours
+  // if there is no data or its been more than 6 hours
   if (
-    latest.creationDate <
-      new Date(Date.now() - 6 * 60 * 60 * 1000).toISOString() ||
-    latest.content === ""
+    (latest &&
+      latest.creationDate <
+        new Date(Date.now() - 6 * 60 * 60 * 1000).toISOString()) ||
+    latest.content === ''
   ) {
-    console.log("Conversion rate is stale");
+    console.log('Conversion rate is stale')
     latest = {
       content: await fetchLatest(),
       creationDate: new Date().toISOString(),
       id: latest.id + 1,
-    };
+    }
+    console.log('***********************')
+    console.log(latest)
   }
+
+  // retrieves the value between the word 'equals' and 'brazillian' // theres a typo there 🤷
+  const rate = latest.content.split('equals')[1].substring(0, 6)
+
   return {
-    source:
-      "https://www.xe.com/currencyconverter/convert/?Amount=1&From=BRL&To=USD",
-    rate: Number(latest.content.split(" ")[3].replace("=", "")),
-    lastUpdateStr: latest.content.split(" — ")[1].split("1.00")[0],
-  };
+    rate: Number(rate),
+    lastUpdateStr: latest.creationDate,
+  }
 }
 
 export async function fetchLatest(): Promise<string> {
-  const webPage = await fetch(
-    "https://www.xe.com/currencyconverter/convert/?Amount=1&From=BRL&To=USD"
-  );
-  const pageContent = await webPage.text();
-  const $ = cheerio.load(pageContent);
-  const content = $('[data-testid="conversion"]').text();
-  insertScrapedData(content);
-  return content;
+  const webPage = await fetch('https://search.brave.com/search?q=usd+to+brl')
+  const pageContent = await webPage.text()
+  const $ = cheerio.load(pageContent)
+  const content = $('div.currency-info')
+    .text()
+    .trim()
+    .slice(0, 100)
+    .toLowerCase()
+  insertScrapedData(content)
+  return content
 }
